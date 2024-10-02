e7ce706ece84bdc69436524ab97af11080a27354
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-02 16:37:47 +0200

    esercizio completo (corretto)

--- resultBadwords.php
+++ resultBadwords.php
@@ -8,5 +8,31 @@
 
     //stampo in pagina il test dell'utente
     //echo(str_ireplace("$userWord", "***", $userWord));
-    echo($userText . " " . str_ireplace("$userWord", "***", $userWord));
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Result</title>
+</head>
+<body>
+    <div>
+        <span>Il testo inserito è:</span>
+        <?php echo($userText . " " . $userWord); ?>
+    </div>
+    <div>
+        <span>La lunghezza del testo è pari a:</span>
+        <?php echo(mb_strlen($userText . $userWord)); ?>
+    </div>
+    <div>
+        <span>Il testo censurato è:</span>
+        <?php echo($userText . " " . str_ireplace("$userWord", "***", $userWord)); ?>
+    </div>
+    <div>
+        <span>La lunghezza del testo censurato è pari a:</span>
+        <?php echo(mb_strlen($userText . " " . str_ireplace("$userWord", "***", $userWord))) ?>
+    </div>
+</body>
+</html>
