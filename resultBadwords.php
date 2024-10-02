5f8f9f9806feab07a8ad32055d9bbcb84089a2e1
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-02 15:50:09 +0200

    added variables in resultBad Words

---
+++ resultBadwords.php
@@ -0,0 +1,9 @@
+<?PHP
+//dichiarazione variabili
+    // # 1. Variabile per il testo
+    $userText = $_GET["userText"];
+    //var_dump($userText);
+    $userWord = $_GET["userWord"];
+    //var_dump($userWord);
+
+?>
